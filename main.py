5a37e1e351a6ab010089721bc1b778f4fc72003c
ramoneirao <ramonneirao@gmail.com>
2026-04-14 18:15:42 -0300

    feat: atualiza para integrar os pipelines de ingestão e validação de dados

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 from src.mlops_wine_prediction import logger
+from src.mlops_wine_prediction.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.mlops_wine_prediction.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> Estágio {STAGE_NAME} iniciado <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.initiate_data_ingestion()
+        logger.info(f">>>>>> Estágio {STAGE_NAME} concluído <<<<<<\n\nx==========x")
+
+    except Exception as e:
+        logger.exception(e)
+        raise e
 
 
-logger.info("Bem vindo ao projeto mlops-wine-prediction!")
+STAGE_NAME = "Data Validation Stage"
+
+try:
+    logger.info(f">>>>>> Estágio {STAGE_NAME} iniciado <<<<<<")
+    obj = DataValidationTrainingPipeline()
+    obj.initiate_data_validation()
+    logger.info(f">>>>>> Estágio {STAGE_NAME} concluído <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
